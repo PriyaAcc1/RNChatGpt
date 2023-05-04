3ffa42f3f1304fad75103dcb5ae7838f29b1d779
PriyaAcc1 <priyanka.prakash@accolitedigital.com>
2023-05-04 12:24:12 +0530

    Modify import and screen names, update registercomponent

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import LoginScreen from './screens/LoginScreen';
-import RegistrationScreen from './screens/RegistrationScreen';
-import CreateChallengeScreen from './screens/CreateChallengeScreen';
-import ViewChallengesScreen from './screens/ViewChallengesScreen';
-import LeaderboardScreen from './screens/LeaderboardScreen';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import LoginScreen from './src/screens/Login';
+import RegistrationScreen from './src/screens/UserRegistration';
+// import CreateChallengeScreen from './screens/CreateChallengeScreen';
+// import ViewChallengesScreen from './screens/ViewChallengesScreen';
+// import LeaderboardScreen from './screens/LeaderboardScreen';
 
 const AppNavigator = createStackNavigator({
-  Login: { screen: LoginScreen },
-  Registration: { screen: RegistrationScreen },
-  CreateChallenge: { screen: CreateChallengeScreen },
-  ViewChallenges: { screen: ViewChallengesScreen },
-  Leaderboard: { screen: LeaderboardScreen },
+  Login: {screen: LoginScreen},
+  Registration: {screen: RegistrationScreen},
+  // CreateChallenge: { screen: CreateChallengeScreen },
+  // ViewChallenges: { screen: ViewChallengesScreen },
+  // Leaderboard: { screen: LeaderboardScreen },
 });
 
-export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);
